cffd050ae7eea2ecd8f65d6982f7dea150954b05
MinJun Kweon <Minz0000000@gmail.com>
2015-11-30 05:30:03 +0900

    Implement login

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,8 +1,15 @@
 package controller;
 
+import model.Student;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		NetworkManager manager = new NetworkManager();
-		manager.login("asdf", "asdf");
+		Student stu;
+		if ((stu = (Student) manager.login("2014112025", "gjsl")) != null) {
+			System.out.println("id : " + stu.getId());
+			System.out.println("name : " + stu.getName());
+			System.out.println("major : " + stu.getMajor());
+		}
 	}
 }
--- src/controller/NetworkManager.java
+++ src/controller/NetworkManager.java
@@ -7,13 +7,19 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import model.Student;
 import model.User;
 
 public class NetworkManager {
-	public static final String API_HOST = "http://eclass.feonfun.com";
-	public static final String USERS = "/users";
+	//public static final String API_HOST = "http://localhost:3001";
+	
+	public static final String API_HOST = "http://www.feonfun.com:8808";
+	public static final String LOGIN = "/login";
 
 	private String accessToken;
 	
@@ -21,8 +27,42 @@ public class NetworkManager {
 		accessToken = null;
 	}
 	
-	public User login(String id, String pw) throws Exception {		
-//		URL obj = new URL(API_HOST + USERS);
+	public User login(String id, String pw) throws Exception {
+		String url = API_HOST + LOGIN;
+		String params = "?id=" + id + "&pw=" + pw;
+		byte[] postData = params.getBytes(StandardCharsets.UTF_8);
+		int    postDataLength = postData.length;           
+		System.out.println(String.valueOf(postDataLength));
+
+		url = url + params;
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		
+		int responseCode = con.getResponseCode();
+		
+		BufferedReader in = new BufferedReader(
+		        new InputStreamReader(con.getInputStream()));
+		String inputLine = in.readLine();
+		in.close();
+		System.out.println("Response Code : " + responseCode);
+		if (responseCode != 200) {
+			JsonObject response = new JsonParser().parse(inputLine).getAsJsonObject();
+			System.out.println(response.get("message").toString());
+			return null;
+		}
+		JsonObject response = new JsonParser().parse(inputLine).getAsJsonObject();
+		JsonObject userJson = response.getAsJsonObject("user");
+		
+		String userId = userJson.get("userId").toString();
+		String name = userJson.get("name").toString();
+		String major = userJson.get("major").toString();
+		Student stu = new Student(userId, name, major);
+		return stu;
+	}
+	
+	public void getLectureOutline() {
+		/*
 		URL obj = new URL("https://cdn.rawgit.com/MinJunKweon/dbit/master/static/1-v1.json");
 		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 		// optional default is GET
@@ -32,12 +72,18 @@ public class NetworkManager {
 		
 		BufferedReader in = new BufferedReader(
 		        new InputStreamReader(con.getInputStream()));
-		String inputLine;
-		inputLine = in.readLine();
+		String inputLine = in.readLine();
 		in.close();
+		
+		JsonArray jsonArray = new JsonParser().parse(inputLine).getAsJsonArray();
 
 		System.out.println("Response Code : " + responseCode);
+		for (JsonElement i : jsonArray) {
+			JsonObject o = i.getAsJsonObject();
+			System.out.println(o);
+		}
 		
 		return stu;
+		*/
 	}
 }
