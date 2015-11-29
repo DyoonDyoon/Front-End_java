e9a17fa7afc9608f68c39bcf55b1df918d5f558d
Naesaori <adsad1002112@naver.com>
2015-11-30 05:31:23 +0900

    Adjust

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,5 +1,7 @@
 package controller;
 
+import controller.NetworkManager;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		NetworkManager manager = new NetworkManager();
